3c473b128e6e0aca00700d50193ed9621d6bf64a
Jeff Hodges <jeff@somethingsimilar.com>
2015-07-30 15:30:23 -0700

    maybe its old git, so just in case

--- test.sh
+++ test.sh
@@ -109,7 +109,7 @@ function build_letsencrypt() {
     $LETSENCRYPT_PATH || exit 1
 
   cd $LETSENCRYPT_PATH
-  git checkout update-challenges
+  run git checkout -b update-challenges origin/update-challenges
 
   run virtualenv --no-site-packages -p $PY ./venv
   run ./venv/bin/pip install -r requirements.txt -e acme -e . -e letsencrypt-apache -e letsencrypt-nginx
