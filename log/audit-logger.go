2adb2a6160ed9f410f3be6272c83aa01548a00f8
Jeff Hodges <jeff@somethingsimilar.com>
2015-08-11 19:23:51 -0700

    try with no panics

--- log/audit-logger.go
+++ log/audit-logger.go
@@ -208,16 +208,16 @@ func caller(level int) string {
 // in a defer statement as early as possible
 // AUDIT[ Error Conditions ] 9cc4d537-8534-4970-8665-4b382abe82f3
 func (log *AuditLogger) AuditPanic() {
-	if err := recover(); err != nil {
-		buf := make([]byte, 8192)
-		log.Audit(fmt.Sprintf("Panic caused by err: %s", err))
+	// if err := recover(); err != nil {
+	// 	buf := make([]byte, 8192)
+	// 	log.Audit(fmt.Sprintf("Panic caused by err: %s", err))
 
-		runtime.Stack(buf, false)
-		log.Audit(fmt.Sprintf("Stack Trace (Current frame) %s", buf))
+	// 	runtime.Stack(buf, false)
+	// 	log.Audit(fmt.Sprintf("Stack Trace (Current frame) %s", buf))
 
-		runtime.Stack(buf, true)
-		log.Warning(fmt.Sprintf("Stack Trace (All frames): %s", buf))
-	}
+	// 	runtime.Stack(buf, true)
+	// 	log.Warning(fmt.Sprintf("Stack Trace (All frames): %s", buf))
+	// }
 }
 
 // WarningErr formats an error for the Warn level.
