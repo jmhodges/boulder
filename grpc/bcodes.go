48fcaf26a8dd7cedf0728350c083f8eef7753e0e
Jeff Hodges <jeff@somethingsimilar.com>
2017-01-27 15:30:22 -0800

    delete some never-used error wrappings
    These error types never make it into the grpc layer.
    
    BadNonceError, LengthRequiredError and SignatureValidationError are in the wfe only.
    
    NotSupportedError is only in the amqp-rpc stack.

--- grpc/bcodes.go
+++ grpc/bcodes.go
@@ -37,20 +37,12 @@ func errorToCode(err error) codes.Code {
 	switch err.(type) {
 	case core.MalformedRequestError:
 		return MalformedRequestError
-	case core.NotSupportedError:
-		return NotSupportedError
 	case core.UnauthorizedError:
 		return UnauthorizedError
 	case core.NotFoundError:
 		return NotFoundError
-	case core.LengthRequiredError:
-		return LengthRequiredError
-	case core.SignatureValidationError:
-		return SignatureValidationError
 	case core.RateLimitedError:
 		return RateLimitedError
-	case core.BadNonceError:
-		return BadNonceError
 	case core.NoSuchRegistrationError:
 		return NoSuchRegistrationError
 	case core.InternalServerError:
@@ -86,24 +78,16 @@ func unwrapError(err error) error {
 	switch code {
 	case InternalServerError:
 		return core.InternalServerError(errBody)
-	case NotSupportedError:
-		return core.NotSupportedError(errBody)
 	case MalformedRequestError:
 		return core.MalformedRequestError(errBody)
 	case UnauthorizedError:
 		return core.UnauthorizedError(errBody)
 	case NotFoundError:
 		return core.NotFoundError(errBody)
-	case SignatureValidationError:
-		return core.SignatureValidationError(errBody)
 	case NoSuchRegistrationError:
 		return core.NoSuchRegistrationError(errBody)
 	case RateLimitedError:
 		return core.RateLimitedError(errBody)
-	case LengthRequiredError:
-		return core.LengthRequiredError(errBody)
-	case BadNonceError:
-		return core.BadNonceError(errBody)
 	case ProblemDetails:
 		pd := probs.ProblemDetails{}
 		if json.Unmarshal([]byte(errBody), &pd) != nil {
